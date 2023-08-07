7810ec447573093591238bcc9fdc5671746b5628
Sugakesshaa <sugakesshaa@gmail.com>
2023-08-07 21:20:18 +0800

    martini: Set screen density according to stock
    Change-Id: Icfd1f372507c11eb5661142f6b341da4b3cd7e31

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ include device/oneplus/sm8350-common/BoardConfigCommon.mk
 DEVICE_PATH := device/oneplus/martini
 
 # Display
-TARGET_SCREEN_DENSITY := 450
+TARGET_SCREEN_DENSITY := 480
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
